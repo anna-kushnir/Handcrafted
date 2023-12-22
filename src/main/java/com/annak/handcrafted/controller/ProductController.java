c91f27bca8f65cdfde3fcd4e131fd6dad80650e0
anna-kushnir <anna.kushnir.1008@gmail.com>
2023-12-22 19:49:52 +0200

    handle exception in ProductController

--- src/main/java/com/annak/handcrafted/controller/ProductController.java
+++ src/main/java/com/annak/handcrafted/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.annak.handcrafted.controller;
 
+import com.annak.handcrafted.dto.ProductDto;
+import com.annak.handcrafted.exception.ResourceNotFoundException;
 import com.annak.handcrafted.service.CategoryService;
 import com.annak.handcrafted.service.ColorService;
 import com.annak.handcrafted.service.ProductService;
@@ -9,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/products")
@@ -23,12 +27,20 @@ public class ProductController {
     public String getAllProducts(Model model) {
         model.addAttribute("categories", categoryService.getAll());
         model.addAttribute("colors", colorService.getAll());
+        model.addAttribute("products", productService.getAll());
         return "user/list_of_products";
     }
 
     @GetMapping("/{id}")
     public String getProductById(@PathVariable Long id, Model model) {
-        model.addAttribute("product", productService.getById(id));
-        return "user/product";
+        Optional<ProductDto> productDtoOptional = productService.getById(id);
+        if (productDtoOptional.isPresent()) {
+            ProductDto productDto = productDtoOptional.get();
+            model.addAttribute("product", productDto);
+            return "user/product";
+        }
+        else {
+            throw new ResourceNotFoundException("Product with id " + id + " was not found");
+        }
     }
 }
