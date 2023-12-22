d384a0a3064487a622e7d28ab0b96af006d1545f
anna-kushnir <anna.kushnir.1008@gmail.com>
2023-12-22 02:41:25 +0200

    added sample data for checking functionality

---
+++ src/main/resources/db/migration/V2__add_sample_data.sql
@@ -0,0 +1,16 @@
+INSERT INTO HANDCRAFTED_SCHEMA.CATEGORY (NAME, DESCRIPTION)
+VALUES
+    ('Hair bands', 'Hair bands decorated with bows and other ornaments'),
+    ('Hair hoops', 'Hair hoops decorated with flowers, animal ears and other ornaments'),
+    ('Hairpins', 'Hairpins decorated with bows and other ornaments');
+
+INSERT INTO HANDCRAFTED_SCHEMA.COLOR (NAME)
+VALUES
+    ('Red'),
+    ('Orange'),
+    ('Yellow'),
+    ('Green'),
+    ('Blue'),
+    ('Purple'),
+    ('White'),
+    ('Black');
