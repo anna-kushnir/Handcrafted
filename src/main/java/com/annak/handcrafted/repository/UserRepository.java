c23d5d45dcd72420abecbe12468c317a074f5f9b
anna-kushnir <anna.kushnir.1008@gmail.com>
2023-12-22 02:40:00 +0200

    added method to UserRepository

--- src/main/java/com/annak/handcrafted/repository/UserRepository.java
+++ src/main/java/com/annak/handcrafted/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.annak.handcrafted.repository;
 import com.annak.handcrafted.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAllByOrderByIdAsc();
 }
