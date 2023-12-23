873b616b220254d53ebeaed6c37069543a13f40e
anna-kushnir <anna.kushnir.1008@gmail.com>
2023-12-23 22:44:13 +0200

    added WebMvcConfigurer

---
+++ src/main/java/com/annak/handcrafted/config/MvcConfiguration.java
@@ -0,0 +1,13 @@
+package com.annak.handcrafted.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/menu").setViewName("user/main_menu");
+    }
+}
