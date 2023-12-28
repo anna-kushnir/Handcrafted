3b6d719606840d746c4bba55e8f69669e3e3986a
anna-kushnir <anna.kushnir.1008@gmail.com>
2023-12-28 14:01:42 +0200

    added entities for Favorite_Product and Product_In_Order tables

---
+++ src/main/java/com/annak/handcrafted/entity/FavoriteProduct.java
@@ -0,0 +1,27 @@
+package com.annak.handcrafted.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "FAVORITE_PRODUCT", schema = "HANDCRAFTED_SCHEMA")
+@Getter
+@Setter
+@EqualsAndHashCode(of = {"user", "product"})
+@NoArgsConstructor
+@AllArgsConstructor
+public class FavoriteProduct {
+    @Id
+    @SequenceGenerator(name = "ID_GENERATOR_FAVORITE_PRODUCT", sequenceName = "HANDCRAFTED_SCHEMA.FAVORITE_PRODUCT_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ID_GENERATOR_FAVORITE_PRODUCT")
+    @Column(name = "ID", updatable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product product;
+}
---
+++ src/main/java/com/annak/handcrafted/entity/ProductInOrder.java
@@ -0,0 +1,30 @@
+package com.annak.handcrafted.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "PRODUCT_IN_ORDER", schema = "HANDCRAFTED_SCHEMA")
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductInOrder {
+    @Id
+    @SequenceGenerator(name = "ID_GENERATOR_PRODUCT_IN_ORDER", sequenceName = "HANDCRAFTED_SCHEMA.PRODUCT_IN_ORDER_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ID_GENERATOR_PRODUCT_IN_ORDER")
+    @Column(name = "ID", updatable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product product;
+
+    @Column(name = "QUANTITY")
+    private Long quantity;
+}
